9a021396027acc6b86770caa1902a8de764b7b18
kriss <kriss@xixs.com>
2015-10-20 20:26:08 +0100

    .

--- box/05-setup-box.sh
+++ box/05-setup-box.sh
@@ -3,9 +3,9 @@ cd `dirname $0`
 echo " building monster-mesh.img from default *mini* raspbian img "
 
 
-cat ~/.ssh/id_rsa.pub | sshpass -p raspbian ssh -p 5522 root@localhost " cat >> .ssh/authorized_keys "
+cat ~/.ssh/id_rsa.pub | sshpass -p raspberry ssh -p 5522 root@localhost " mkdir -p .ssh ; cat >> .ssh/authorized_keys "
 
 
 
-#ssh -p 5522 root@localhost -c " ls "
+#ssh -p 5522 root@localhost " ls "
 
