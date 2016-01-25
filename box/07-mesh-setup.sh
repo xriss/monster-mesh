1eadc29af0aedcf74e2e83d2977fa7ea6d23afed
kriss <kriss@xixs.com>
2016-01-25 03:35:48 +0000

    add mercurial and clone my mmesh repos

--- box/07-mesh-setup.sh
+++ box/07-mesh-setup.sh
@@ -2,6 +2,15 @@ cd `dirname $0`
 
 echo " Start doing things that are unique to monster mesh "
 
+
+echo " grab the monster-mesh git repo"
+./ssh " git clone https://github.com/xriss/monster-mesh "
+
+
+echo " grab the gamecake engine binary repo"
+./ssh " hg clone https://bitbucket.org/xixs/bin "
+
+
 echo " clone, build and install olsrd2 "
 ./ssh " git clone git://olsr.org/oonf.git && cd oonf/build && cmake -D OONF_NO_WERROR=true .. && make && sudo make install "
 
