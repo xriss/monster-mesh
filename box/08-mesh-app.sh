7dbefce9984e85dcf0700c408715ae586af55094
kriss <kriss@xixs.com>
2016-02-04 21:22:55 +0000

    .

--- box/08-mesh-app.sh
+++ box/08-mesh-app.sh
@@ -8,7 +8,7 @@ echo " Building the mmesh app "
 echo " Copying the mmesh app "
 
 scp -P 5522 ./adhoc-config pi@localhost:
-scp -P 5522 ./pi-start-mesh pi@localhost:
+scp -P 5522 ./pi-start pi@localhost:
 
 scp -P 5522 ../mmesh/out/mmesh.zip pi@localhost:
 scp -P 5522 ../../bin/exe/gamecake.raspi pi@localhost:
